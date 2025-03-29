0dff6810d316fbd145be2c36660031992ed51ae8
DESKTOP-20BACJI\Admin <binh.ngv.dev@gmail.com>
2025-03-29 19:46:15 +0700

    init launch.setting

--- src/collections/Post.ts
+++ src/collections/Post.ts
@@ -1,10 +1,26 @@
 import type { CollectionConfig } from "payload";
 
 export const Post: CollectionConfig = {
-  slug: "post",
-  admin: {
-    useAsTitle: "email",
-  },
-  auth: true,
+  slug: "posts",
   fields: [{ name: "title", type: "text" }],
+  dbName: "posts",
+  timestamps: true,
+  versions: {
+    maxPerDoc: 0,
+    drafts: true
+  },
+  access: {
+    read: ( request ) => {
+      console.log(request.req.user)
+      if(request.req.user){
+        
+        return true
+      }
+      return false
+    },
+
+    readVersions: (request)=>{
+      return Boolean(request.req.user)
+    }
+  }
 };
--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -9,5 +9,12 @@ export const Users: CollectionConfig = {
   fields: [
     // Email added by default
     // Add more fields as needed
+    {
+      type: 'select',
+      name: 'role',
+      options: ['super-admin', 'admin', 'user'],
+      required: true
+    }
   ],
 }
+export type UserData = typeof Users.fields
